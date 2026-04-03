a0c44864db8d55a9aacedbc6e75b47b7b413608c
YEVHENII ANDROSOV <j.a.androsov@gmail.com>
2026-04-03 12:45:17 +0300

    Зміни Branch

---
+++ src/main/java/ua/edu/duan/warehouse/controller/WarehouseController.java
@@ -0,0 +1,19 @@
+package ua.edu.duan.warehouse.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+
+public class WarehouseController {
+      @GetMapping("/hello-world")
+    public String sayHelloWorld(){
+        return "Hello World - advanced logic";
+    }
+
+}
