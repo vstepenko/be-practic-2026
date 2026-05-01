caf49db4014824ef2f21d05c7b3b26e0e2dcb93e
YEVHENII ANDROSOV <j.a.androsov@gmail.com>
2026-05-01 11:45:26 +0300

    Added item redactor

--- src/main/java/ua/edu/duan/warehouse/controller/WarehouseController.java
+++ src/main/java/ua/edu/duan/warehouse/controller/WarehouseController.java
@@ -3,9 +3,11 @@ package ua.edu.duan.warehouse.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +16,7 @@ import ua.edu.duan.warehouse.dao.entity.CatalogEntity;
 import ua.edu.duan.warehouse.dao.repository.CatalogRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -50,5 +53,24 @@ public class WarehouseController {
         entity.setIcon(itemDto.getIcon());
         entity.setAttributes(itemDto.getAttributes());
         catalogRepository.save(entity);
+    }
+
+    @PutMapping("/item/{id}")
+    public void updateItem(@PathVariable String id, @RequestBody ItemDto itemDto) {
+        Optional<CatalogEntity> existingOpt = catalogRepository.findById(id);
+        if (existingOpt.isEmpty()) {
+            throw new RuntimeException("Item not found");
+        }
+        CatalogEntity entity = existingOpt.get();
+        entity.setItemName(itemDto.getItemName());
+        entity.setDescription(itemDto.getDescription());
+        entity.setIcon(itemDto.getIcon());
+        entity.setAttributes(itemDto.getAttributes());
+        catalogRepository.save(entity);
+    }
+
+    @DeleteMapping("/item/{id}")
+    public void deleteItem(@PathVariable String id) {
+        catalogRepository.deleteById(id);
     }
 }
